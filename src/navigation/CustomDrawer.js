987dbcfc799bc8b5df3becf5fb4aa2f0fc100917
Joanne0119 <liuchengen52033@gmail.com>
2024-03-17 22:55:06 +0800

    edit style to fit android

--- src/navigation/CustomDrawer.js
+++ src/navigation/CustomDrawer.js
@@ -1,14 +1,14 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 
 const CustomDrawer = (props) => {
   return (
     <DrawerContentScrollView {...props} >
-        <View style={{marginVertical:20, paddingBottom: 30, paddingHorizontal: 30, borderBottomColor: '#eee', borderBottomWidth: 1}}>
+        <View style={styles.container}>
             <Image 
                 source = {{uri: 'https://github.com/Joanne0119/book_app/blob/master/assets/image/img_avatar.png?raw=true'}}
-                style={{width: 52, height: 52, borderRadius: '100%', marginBottom: 15}}
+                style={styles.image}
             />
             <Text style={{fontSize: 24, fontWeight: 'bold'}}>May</Text>
         </View>
@@ -17,5 +17,21 @@ const CustomDrawer = (props) => {
     </DrawerContentScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: 52, 
+        height: 52, 
+        borderRadius: 100, 
+        marginBottom: 15
+    },
+    container: {
+        marginVertical:20, 
+        paddingBottom: 30, 
+        paddingHorizontal: 30, 
+        borderBottomColor: '#eee', 
+        borderBottomWidth: 1
+    }
+  });
 
 export default CustomDrawer
