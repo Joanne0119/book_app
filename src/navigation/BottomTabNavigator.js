e6ac6a8210a9853f6d66040bfc48bc54c8231068
Joanne0119 <liuchengen52033@gmail.com>
2024-03-17 21:07:49 +0800

    add tab nav icon

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,17 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import WishlistStackNavigator from './WishlistStackNavigator';
 import HomeStackNavigator from './HomeStackNavigator';
 import MyBookStackNavigator from './MyBookStackNavigator';
-import TabBar from '../components/TabBar';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator tabBar={props => <TabBar {...props} />}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, size, color }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+            size = focused ? 25 : 20;
+            // color = focused ? '#f0f' : '#555';
+          } else if (route.name === 'Wishlist') {
+            iconName = 'wishlist';
+            size = focused ? 25 : 20;
+            // color = focused ? '#f0f' : '#555';
+          } else if (route.name === 'My Books') {
+            iconName = 'my_books';
+            size = focused ? 25 : 20;
+            // color = focused ? '#f0f' : '#555';
+          }
+          return (
+            <Image
+              source = {{uri: focused ? `https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/icon_${iconName}_actived.png?raw=true`:`https://github.com/Joanne0119/book_app/blob/master/assets/image/icon/icon_${iconName}_actived.png?raw=true`}}
+              style={{width: 25, height: 25, marginTop: 12}}
+            />
+          )
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: '#6200EE',
+        inactiveTintColor: '#666',
+        showLabel: true,
+        labelStyle: { fontSize: 14, fontWeight: 500, marginTop: 5},
+        showIcon: true,
+      }}
+    >
       <Tab.Screen name="Home" component={HomeStackNavigator} options={{headerShown: false}}/>
       <Tab.Screen name="Wishlist" component={WishlistStackNavigator} options={{headerShown: false}}/>
       <Tab.Screen name="My Books" component={MyBookStackNavigator} options={{headerShown: false}}/>
